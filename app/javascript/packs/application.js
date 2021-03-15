9fc080519e3c747d472e7d2424a1f6941d120952
David <sanchez.dav90@gmail.com>
2021-03-14 22:46:21 -0600

    Add Home from Inertiajs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,5 +10,23 @@ import "channels"
 Rails.start()
 ActiveStorage.start()
 
-import "@fortawesome/fontawesome-free/css/all"
-import "stylesheets/application"
+import Vue from 'vue'
+import { App, plugin } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+import '@fortawesome/fontawesome-free/css/all'
+import 'stylesheets/application'
+
+Vue.use(plugin)
+InertiaProgress.init()
+
+const el = document.getElementById('app')
+
+new Vue({
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`../pages/${name}`).default,
+    },
+  }),
+}).$mount(el)
