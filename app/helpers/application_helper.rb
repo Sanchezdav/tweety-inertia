807c4106757cfe9a1b784bfe09b1effad957b4bb
David <sanchez.dav90@gmail.com>
2021-03-14 20:36:03 -0600

    Add gravatar to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
   ActionView::Base.default_form_builder = TailwindFormBuilder
+
+  def user_gravatar(user, size)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, width: size, height: size, class: 'rounded-full')
+  end
 end
