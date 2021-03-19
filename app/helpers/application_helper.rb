5c215bc7a63c5d7f3b217d6e2c73db276367c142
David <sanchez.dav90@gmail.com>
2021-03-18 20:45:02 -0600

    Add more elements to sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def user_gravatar(user, size)
     image_tag(user.gravatar_url, alt: user.name, width: size, height: size, class: 'rounded-full')
   end
+
+  def current_path?(current_path)
+    return 'active' if request.path == current_path
+
+    ''
+  end
 end
